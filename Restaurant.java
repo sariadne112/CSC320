cb5f68435232a3d62e6f2f60b402bf072015b3dc
Ariadne Nava <ariadneg112@gmail.com>
2026-06-14 00:49:02 -0400

    first commit Restaurant

---
+++ Restaurant.java
@@ -0,0 +1,8 @@
+public class Restaurant {
+  public static void main(String[] args){
+
+    System.out.println("The Restaurant Name is: Cafe Girasol");  /* Print Restaurant name */
+    System.out.println("Address: 845 Water Street ");  /*Print Street address */
+    System.out.println("City: Oakland Sate: Ca  Zip: 94607"); /*Print City, State and Zip */
+  }
+}
