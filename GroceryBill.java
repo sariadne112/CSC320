6e8dc42f253e30fb470689918fda4563683af6ba
Ariadne Nava <ariadneg112@gmail.com>
2026-06-27 01:22:02 -0400

    first commit GroceryBill

---
+++ GroceryBill.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class GroceryBill {
+  public static void main (String [] args){
+    Scanner scnr = new Scanner(System.in);
+    double coupon;
+    double week1;
+    double week2;
+    double week3;
+    double week4;
+    double avgWeekly;
+    double avgMonthly;
+    double totalMonthly;
+    double monthlyWithCoupon;
+    double weekWithCoupon;
+
+    System.out.print("Enter Coupon Amount in decimal form (example, .10) ");
+    coupon = scnr.nextDouble();
+
+    
+
+  }
+}
