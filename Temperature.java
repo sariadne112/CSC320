1b1fa9b76765b59aba45dc0378ae7c664041e0e3
Ariadne Nava <ariadneg112@gmail.com>
2026-07-11 13:10:04 -0400

    temperature code added more loops to validate

--- Temperature.java
+++ Temperature.java
@@ -1,37 +1,94 @@
 import java.util.Scanner;
 
 public class Temperature {
-  public static void main (String [] args){
-    Scanner scnr = new Scanner(System.in);
-    int[] dayOfWeek = new int[7];
-    double[] temperature = new double[7];
-    int searchDay;
-    int i;
-    
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
 
-//Day of week for loop
-  for (i = 0; i < 7; i++){
-    System.out.println("Please insert weekday: ");
-    dayOfWeek[i] = scnr.nextInt();
+        String[] dayOfWeek = new String[7];
+        double[] temperature = new double[7];
 
-    if (dayOfWeek.equalsIgnoreCase("Monday") ||
-    dayOfWeek.equalsIgnoreCase("Tuesday") ||
-    dayOfWeek.equalsIgnoreCase("Wednesday") ||
-    dayOfWeek.equalsIgnoreCase("Thursday") ||
-    dayOfWeek.equalsIgnoreCase("Friday") ||
-    dayOfWeek.equalsIgnoreCase("Saturday") ||
-    dayOfWeek.equalsIgnoreCase("Sunday")) {
-      temperature[i] = scnr.nextDouble();
+        String weekday;
+        int i;
+        int j;
+
+        // Collect seven weekdays and temperatures
+        for (i = 0; i < dayOfWeek.length; i++) {
+
+            System.out.print("Please enter weekday: ");
+            weekday = scnr.next();
+
+            boolean duplicateDay = false;
+
+            for (j = 0; j < i; j++) {
+          if (weekday.equalsIgnoreCase(dayOfWeek[j])) {
+             duplicateDay = true;
+          }
+        }
+
+          if (duplicateDay) {
+           System.out.println("That weekday has already been entered.");
+            i--;
+          continue;
+        }
+
+
+            // Validate weekday
+            if (weekday.equalsIgnoreCase("Monday") ||
+                weekday.equalsIgnoreCase("Tuesday") ||
+                weekday.equalsIgnoreCase("Wednesday") ||
+                weekday.equalsIgnoreCase("Thursday") ||
+                weekday.equalsIgnoreCase("Friday") ||
+                weekday.equalsIgnoreCase("Saturday") ||
+                weekday.equalsIgnoreCase("Sunday")) {
+
+                dayOfWeek[i] = weekday;
+
+                boolean validTemperature = false;
+
+                // Validate temperature
+                while (!validTemperature) {
+
+                    System.out.print("Enter temperature in Fahrenheit: ");
+
+                    if (scnr.hasNextDouble()) {
+                        temperature[i] = scnr.nextDouble();
+
+                        if (temperature[i] >= -20 &&
+                            temperature[i] <= 130) {
+
+                            validTemperature = true;
+
+                        } else {
+                            System.out.println(
+                                "Invalid temperature. Enter a value between -20 and 130."
+                            );
+                        }
+
+                    } else {
+                        System.out.println(
+                            "Invalid input. Please enter a number."
+                        );
+
+                        scnr.next(); // Remove invalid input
+                    }
+                }
+
+            } else {
+                System.out.println("Please enter a valid weekday.");
+                i--; // Repeat this array position
+            }
+        }
+
+        // Print the entire week
+        System.out.println("\nWeekly Temperatures:");
+
+        for (i = 0; i < dayOfWeek.length; i++) {
+            System.out.println(
+                "Day: " + dayOfWeek[i] +
+                " Temperature: " + temperature[i]
+            );
+        }
+
+        scnr.close();
     }
-
-    // Valid day
-}
-  //test print
-  System.out.println(dayOfWeek);
-
-
-
-
-
-  }
 }
